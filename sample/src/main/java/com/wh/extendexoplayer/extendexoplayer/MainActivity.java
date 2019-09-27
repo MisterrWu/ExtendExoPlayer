a4c9ba8c09fc3415519eab739d89e8752eac4e5f
wuhan <han.wu-app@lumiunited.com>
2019-09-27 11:57:16 +0800

    Feature: add layout activity_main

--- sample/src/main/java/com/wh/extendexoplayer/extendexoplayer/MainActivity.java
+++ sample/src/main/java/com/wh/extendexoplayer/extendexoplayer/MainActivity.java
@@ -2,11 +2,22 @@ package com.wh.extendexoplayer.extendexoplayer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.wh.extendexoplayer.renderers.NormalRectRenderer;
+import com.wh.extendexoplayer.widget.RendererView;
+import com.wh.extendexoplayer.widget.TextureRendererView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextureRendererView textureView = findViewById(R.id.bt_view);
+        RendererView.Renderer renderer = new NormalRectRenderer(true);
+        textureView.setRenderer(renderer);
     }
 }
