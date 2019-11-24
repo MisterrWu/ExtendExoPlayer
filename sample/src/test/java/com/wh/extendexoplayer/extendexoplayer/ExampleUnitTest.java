0302620ce39dd80af72c564c1225ee88d7f1f2b8
wuhan <han.wu-app@lumiunited.com>
2019-11-24 10:06:18 +0800

    Feature: exampleUnitTest add method bubbleSort

--- sample/src/test/java/com/wh/extendexoplayer/extendexoplayer/ExampleUnitTest.java
+++ sample/src/test/java/com/wh/extendexoplayer/extendexoplayer/ExampleUnitTest.java
@@ -12,6 +12,36 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        int[] arr = {1, 2, 4, 5, 3, 4, 6, 1};
+        bubbleSort(arr);
+        print(arr);
+    }
+
+    private void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) { // 8,7,6,5,4,3,2,1,0
+            // 1(7-0)
+            // 2(6-0)
+            // 3(5-0)
+            // .
+            // .
+            // .
+            for (int j = 0; j < (arr.length - i) - 1; j++) { // 每趟循环后都有一个最大值排到后面，减一是因为最后一个值没有比较了。
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private void print(int[] arr) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            builder.append(arr[i]).append((i != arr.length - 1) ? ", " : "");
+        }
+        builder.append("]");
+        System.out.println(builder.toString());
     }
 }
